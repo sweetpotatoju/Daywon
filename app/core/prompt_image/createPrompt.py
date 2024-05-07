8bc82fab7aac9a8a158effa4c6fae75140924cd4
jinhui-leee <fkdkfk1578@gmail.com>
2024-05-07 17:23:54 +0900

    프롬프트 수정

--- app/core/prompt_image/createPrompt.py
+++ app/core/prompt_image/createPrompt.py
@@ -30,8 +30,8 @@ async def create_prompt(finance_category):
 async def create_video_prompt(conceptual_prompt):
     api_key = get_api_key()
     model = 'gpt-4'
-    system_prompt = f"{conceptual_prompt}에 대한 내용을 바탕으로 예시를 들어 각 문장의 글자 수가 50자 이내인 총 6문장으로 설명하고, 알아 듣기 쉽게 반말로 알려줍니다. 온점은 오로지 문장이 끝났을 때만 사용합니다."
-    user_prompt = f"{conceptual_prompt}에 대한 구체적 예시를 들고 알아 듣기 쉽게 설명해줘"
+    system_prompt = f"각 문장의 글자 수가 80자 이내인 총 6문장으로 설명하고, 어린 아이에게 이야기하는 느낌으로 말한다. 온점은 오로지 문장이 끝났을 때만 사용합니다."
+    user_prompt = f"{conceptual_prompt}에 대한 구체적인 실생활 예시를 들고 알아 듣기 쉽게 설명해줘"
     api_url, headers, data = util_api(api_key, model, system_prompt, user_prompt)
     return await call_api(api_url, headers, data)
 
