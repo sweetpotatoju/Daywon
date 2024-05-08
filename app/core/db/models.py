b5b17fee5a25ec44a7fcd999ee6f04d399b2c409
jinhui-leee <fkdkfk1578@gmail.com>
2024-05-08 23:43:48 +0900

    Scripts model 추가

--- app/core/db/models.py
+++ app/core/db/models.py
@@ -17,10 +17,22 @@ class User(Base):
 
 class Item(Base):
     __tablename__ = "items"
-
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String, index=True)
     owner_id = Column(Integer, ForeignKey("users.id"))
 
     owner = relationship("User", back_populates="items")
+
+
+class Scripts(Base):
+    __tablename__ = "scripts"
+    scripts_id = Column(Integer, primary_key=True, index=True)
+    level = Column(Integer, index=True)
+    category_name = Column(String, index=True)
+    content_1 = Column(String, index=True)
+    content_2 = Column(String, index=True)
+    content_3 = Column(String, index=True)
+
+
+
