f8060cd5b0e0851533f633e4b552fdccf338ae6d
guddnboy <hung3018@naver.com>
2024-05-15 04:49:12 +0900

    테스트 코드 수정(함수 이름 수정, 출력 결과물 구분 잘 되게 수정)

--- app/core/prompt_image/test_createPrompt.py
+++ app/core/prompt_image/test_createPrompt.py
@@ -1,28 +1,28 @@
 import unittest
 from unittest import TestCase
-from app.core.prompt_image.createPrompt import create_prompt, create_video_prompt
+from app.core.prompt_image.createPrompt import create_prompt, create_example_prompt
 from app.core.problem.createProblem import create_problem
 from app.core.prompt_image.createImage import generate_images
-from app.core.prompt_image.createPrompt import split_text
+from app.core.prompt_image.createPrompt import split_text, split_text_two
 
 
 class Test(unittest.IsolatedAsyncioTestCase):
-    async def test_create_video_prompt(self,finance_category=None):
+    async def test_createPrompts(self,finance_category=None):
 
         result = await create_prompt(finance_category)
         self.assertIsNotNone(result)
-        print(f"생성된 개념 스크립트 : {result}")
+        print(f"\n생성된 개념 스크립트\n\n {result}")
 
-        example = await create_video_prompt(result)
-        print(f"생성된 예시 스크립트 : {example}")
+        example = await create_example_prompt(result)
+        print(f"\n생성된 예시 스크립트\n\n {example}")
 
         problem = await create_problem(result, example)
-        print(f"생성된 문제 : {problem}")
+        print(f"\n생성된 문제\n\n {problem}")
 
-        scripts = split_text(example)
-        print(scripts)
-
-        clips_info = []
-        makeImg = await generate_images(scripts, clips_info)
-
-        print(clips_info)
+        # scripts = split_text(example)
+        # scripts = split_text_two(example)
+        # print(scripts)
+        #
+        # clips_info = []
+        # makeImg = await generate_images(scripts, clips_info)
+        # print(clips_info)
